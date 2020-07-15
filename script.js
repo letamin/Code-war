75b92436264046edb7499ee74aacb9e69553f613
letamin <lethanhminh0901@gmail.com>
2020-07-16 01:32:30 +0700

    daily coding challenge

--- script.js
+++ script.js
@@ -1,15 +1,2 @@
-function findEvenIndex(arr) {
-    if (sum(arr) == 0) return 0;
-    for (let i = 0; i < arr.length; i++) {
-        var left = sum(arr.slice(0, i));
-        var right = sum(arr.slice(i + 1));
-        if (left == right) return i;
-    }
-    return -1;
-}
 
-function sum(arr) {
-    return arr.reduce((sum, index) => sum + index, 0)
-}
 
-console.log(findEvenIndex([20, 10, -80, 10, 10, 15, 35]))
